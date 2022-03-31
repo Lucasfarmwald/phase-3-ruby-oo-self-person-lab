321e841fb66986cff068eb562adf1ccd8c0ff0de
lucasfarmwald <farmwaldlucas@gmail.com>
2022-03-31 15:50:39 -0500

    init commit

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,13 @@
 # your code goes here
+class Person
+    attr_reader :name
+    def initilize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+
+    end
+        
+    
+end
