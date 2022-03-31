152e54fe12f119053cdd4b8ee67b20375d587c20
lucasfarmwald <farmwaldlucas@gmail.com>
2022-03-31 17:08:57 -0500

    commit

--- lib/person.rb
+++ lib/person.rb
@@ -1,18 +1,18 @@
 # your code goes here
 class Person
-    attr_reader :name
-    attr_accessor :bank_account, :happiness, :hygiene
+    attr_reader :name, :happiness, :hygiene
+    attr_accessor :bank_account
     def initialize(name)
         @name = name
         @bank_account = 25 
         @happiness = 8 
         @hygiene = 8
     end
-    def happiness(happiness)
-        @happiness = happiness
-        happiness.min = 0
-        happiness.max = 10
+    def happiness(num)
     end
+        
+
+    
 
         
     
