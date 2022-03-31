8be5daca12e5abf95d705b21d60b061e10f1409a
lucasfarmwald <farmwaldlucas@gmail.com>
2022-03-31 17:08:49 -0500

    added happiness method

--- lib/person.rb
+++ lib/person.rb
@@ -1,13 +1,19 @@
 # your code goes here
 class Person
     attr_reader :name
-    def initilize(name)
+    attr_accessor :bank_account, :happiness, :hygiene
+    def initialize(name)
         @name = name
-        @bank_account = 25
-        @happiness = 8
+        @bank_account = 25 
+        @happiness = 8 
         @hygiene = 8
-
     end
+    def happiness(happiness)
+        @happiness = happiness
+        happiness.min = 0
+        happiness.max = 10
+    end
+
         
     
 end
