40e4ce95c6123c75a2af508aa5ea11071105b644
lucasfarmwald <farmwaldlucas@gmail.com>
2022-03-31 20:15:31 -0500

    made changes to take bath

--- lib/person.rb
+++ lib/person.rb
@@ -39,9 +39,8 @@ class Person
         return "all about the benjamins"
     end
     def take_bath
-        @hygiene += 4
+        self.hygiene += 4
 
-        
 
         return "♪ Rub-a-dub just relaxing in the tub ♫"
 
