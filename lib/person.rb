c10ec7ba23166fca97ad704a809544e9e1758529
lucasfarmwald <farmwaldlucas@gmail.com>
2022-04-01 01:15:24 -0500

    final commit

--- lib/person.rb
+++ lib/person.rb
@@ -42,6 +42,31 @@ class Person
         self.hygiene += 4
         return "♪ Rub-a-dub just relaxing in the tub ♫"
     end
+    def work_out
+        self.happiness += 2
+        self.hygiene -= 3
+        return "♪ another one bites the dust ♫"
+    end
+    def call_friend(friend)
+        self.happiness += 3 
+        friend.happiness += 3
+        return "Hi #{friend.name}! It's #{self.name}. How are you?"
+    end
+    def start_conversation(friend, topic)
+        case topic
+        when "politics"
+            [self, friend].each {|person| person.happiness -= 2}
+            "blah blah partisan blah lobbyist"
+        when "weather"
+            [self, friend].each {|person| person.happiness += 1}
+            "blah blah sun blah rain"
+        else
+            "blah blah blah blah blah"   
+        end
+    end
+
+
+
 
     
 
